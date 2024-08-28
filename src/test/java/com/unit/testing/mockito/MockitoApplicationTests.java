a79c48a3a495c5e1eecff92cda7f3a4d8e6cfb77
Najlae Abarghache <najlae.abarghache1@gmail.com>
2024-08-29 00:20:15 +0100

    first commit

---
+++ src/test/java/com/unit/testing/mockito/MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unit.testing.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
